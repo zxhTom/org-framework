f763221ab54b823352bd993ba166265d120a98b7
zxhtoms <zhangxinhua@linyang.com.cn>
2025-11-20 17:03:26 +0800

    init

---
+++ org-framework-basics/org-framework-components/org-framework-web/src/main/java/org/github/zxhtom/App.java
@@ -0,0 +1,13 @@
+package org.github.zxhtom;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
